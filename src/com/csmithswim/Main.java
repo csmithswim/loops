8b9c1818920d0080086f548586c68d7040680f03
Cullen S <cullensmi@gmail.com>
2020-12-03 12:13:20 -0500

    first commit

---
+++ src/com/csmithswim/Main.java
@@ -0,0 +1,22 @@
+package com.csmithswim;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Main {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
